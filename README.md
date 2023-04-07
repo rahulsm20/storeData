f3bf810183ed8d36940713a2db45f0ef72833872
Rahul <rahulsm.1503@gmail.com>
2023-04-07 22:12:11 +0530

    charts and readme update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # SuperStore Data Analysis
+## [Website](https://rahulsm20-storedata-main-t64r4t.streamlit.app)
+
+### In this project, we have aimed to analyse the sales data of the super store and provide useful insight into customer preferences.
+
+## About the dataset
+The dataset used for this analysis is an excel file containing the following columns:
+- Row ID
+- Order ID
+- Order Date
+- Ship Date
+- Ship Mode
+- Customer ID
+- Customer Name
+- Segment
+- Country
+- City
+- State
+- Postal Code
+- Region
+- Product ID
+- Category
+- Sub-Category
+- Product Name
+- Sales
+- Quantity
+- Discount
+- Profit
+
+## Methodology 
+The analysis was conducted in Python using the following libraries:
+* Pandas
+* Numpy
+* Matplotlib
+* Streamlit (for deployment)
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 import streamlit as st 
 import matplotlib.pyplot as plt
+import altair as alt
+from ing_theme_matplotlib import mpl_style
 df=pd.read_excel('./superStore.xls')
 
-df= df.drop(['Order ID','Row ID','Ship Date','Order Date',
+mpl_style('dark')
+df= df.drop(['Order ID','Row ID',
              'Customer ID','Customer Name','Product Name',
-             'Quantity','Postal Code','Product ID'],axis=1)
+             'Postal Code','Product ID'],axis=1)
 
 st.write("""
          # SuperStore Data Analysis 
@@ -25,7 +28,8 @@ st.markdown("""
             \n - [Sales by Category](#sales-by-category)
             \n - [Sales distribution by segment](#sales-by-segment) 
             \n - [Sales distribution by Sub-Category](#sales-distribution-by-sub-category)
-            \n - [Conclusions](#conclusions)
+            \n - [Sales by Date](#sales-by-date)
+            \n - [Results](#results)
             """)
 df['Sales']=df['Sales'].round(1)
 salesByState=df.groupby('State')['Sales'].sum()
@@ -39,6 +43,9 @@ st.write("""
          #### Sales by Category
          """)
 st.bar_chart(salesByCategory)
+fig,ax=plt.subplots()
+ax.pie(salesByCategory,labels=salesByCategory.keys(),autopct='%1.1f%%')
+st.pyplot(fig)
 
 st.write("""
          #### Sales distribution by Ship Mode    
@@ -77,10 +84,44 @@ fig,ax=plt.subplots()
 ax.pie(subcategory,labels=subcategory.keys(),autopct='%1.1f%%',explode=explode)
 st.pyplot(fig)
 
-st.write('## Conclusions')
-st.markdown("From our analysis, we can derive several conclusions.  \n * The stores makes the most revenue in terms of sales from the state of California, followed by New York, Texas, Washington and Pennsylvania in that order.")
-st.markdown("* Over 50% of the stores revenue comes from consumer goods, followed by corporate and home-office in that order.")
-st.markdown("* The store offers multiple types of by shipping modes, but the standard class is by far the most popular.")
-st.markdown("* The store has branches all over the country and the sales distribution is well spread out. The West brings in the most though, at 31.6% followed by East, Central and South.")
+st.write('### Profit by Sub-Category')
+subcategory=df.groupby('Sub-Category')['Profit'].sum()
+st.bar_chart(subcategory)
+
+st.write('### Sales by Date')
+date=df.groupby('Order Date')['Sales'].sum()
+st.write(date)
+st.area_chart(date,y='Sales')
+
+df.set_index(['Category', 'Order Date'], inplace=True)
+categoryDateSales=df.groupby(['Category','Order Date'])['Sales'].sum().reset_index()
+st.altair_chart(alt.Chart(categoryDateSales).mark_area().encode(
+    x='Order Date:T',
+    y='Sales:Q',
+    color='Category:N',
+    tooltip=['Category:N', 'Order Date:T','Sales:Q']
+),use_container_width=True)
+
+st.write('### Profit by Date')
+
+date=df.groupby('Order Date')['Profit'].sum()
+st.write(date)
+st.line_chart(date,y='Profit')
+
+categoryDate=df.groupby(['Category','Order Date'])['Profit'].sum().reset_index()
+st.altair_chart(alt.Chart(categoryDate).mark_area().encode(
+    x='Order Date:T',
+    y='Profit:Q',
+    color='Category:N',
+    tooltip=['Category:N', 'Order Date:T','Profit:Q']
+),use_container_width=True)
 
 
+st.write('### Results')
+st.markdown("""  
+\n * The stores makes the most revenue in terms of sales from the state of California, followed by New York, Texas, Washington and Pennsylvania in that order.
+* Over 50% of the store's revenue comes from consumer goods, followed by corporate and home-office in that order.
+* The store offers multiple types of shipping modes, but the standard class is by far the most popular.
+* The store has branches all over the country and the sales distribution is well spread out. The West brings in the most though, at 31.6% followed by East, Central and South.
+* Copiers are the most profitable sub-category of products, whereas tables are the least profitable, actually producing a net loss.
+* Continuing with sub-categories, Phones bring in the most revenue at 14.4% followed closely by Chairs at 14.3%""")
